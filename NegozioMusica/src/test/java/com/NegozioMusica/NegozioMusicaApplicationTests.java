a65bc90022905e5d28e48170f0987005b950045a
Gavvio <steve.gavvio@gmail.com>
2023-03-22 21:26:21 +0100

    daje

---
+++ NegozioMusica/src/test/java/com/NegozioMusica/NegozioMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NegozioMusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegozioMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
