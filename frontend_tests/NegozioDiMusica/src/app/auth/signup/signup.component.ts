0fbb55acfbdc5e131cb2292f2e8e361987c2dca6
Stefano Gavioli <steve.gavvio@gmail.com>
2023-03-25 15:38:55 +0100

    avanzamento angular

---
+++ frontend_tests/NegozioDiMusica/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent {
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+  }
+}
